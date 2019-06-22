49c61a71cb8a6b15eb14651e16d7eca3eebdaa17
Luis Aponte <luemapont@gmail.com>
2019-06-22 08:32:15 -0400

    Adding components for Helmet

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import React, { Component } from "react";
+import {Helmet} from "react-helmet";
 //import { MDBContainer } from "mdbreact";
 import NavbarComponent from './components/NavbarComponent';
 import TopbarComponent from './components/TopbarComponent';
 import HeaderComponent from './components/HeaderComponent';
 
+var title = 'The Realstate Project | ';
+var subtitle = 'Buy your dreams!';
+//var site_url = window.location.href;
+var page_title = title + subtitle;
+
 class App extends Component {
   render() {
     return (
         <div>
-          <NavbarComponent />
-          <HeaderComponent/>
-          <TopbarComponent />
+            <Helmet>
+                <meta charSet="utf-8" />
+                <title>{ page_title }</title>
+                <meta name="description" content="testing react helmet" />
+                <meta name="keywords" cpntent="react,seo,helmet" />
+            </Helmet>
+            <NavbarComponent />
+            <HeaderComponent/>
+            <TopbarComponent />
         </div>
     );
   }
