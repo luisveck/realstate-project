41e8a0ca3edaa5459a77998c81eeb310c29e6f20
Luis Aponte <luemapont@gmail.com>
2019-06-25 07:26:53 -0400

    Router config for navs

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
+//React imports
 import React, { Component } from "react";
 import {Helmet} from "react-helmet";
-//import { MDBContainer } from "mdbreact";
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+//Pages Components
+import AboutComponent from './components/pagescomponents/AboutComponent';
+
+//Elements Components
 import NavbarComponent from './components/NavbarComponent';
 import TopbarComponent from './components/TopbarComponent';
 import HeaderComponent from './components/HeaderComponent';
@@ -13,19 +19,41 @@ var page_title = title + subtitle;
 class App extends Component {
   render() {
     return (
-        <div>
-            <Helmet>
-                <meta charSet="utf-8" />
-                <title>{ page_title }</title>
-                <meta name="description" content="testing react helmet" />
-                <meta name="keywords" cpntent="react,seo,helmet" />
-            </Helmet>
-            <NavbarComponent />
-            <HeaderComponent/>
+        <Router>
+
+            <div>
+                <Helmet>
+                    <meta charSet="utf-8" />
+                    <title>{ page_title }</title>
+                    <meta name="description" content="Realstate project made with React Js and Love." />
+                    <meta name="keywords" cpntent="realstate" />
+                </Helmet>
+                <NavbarComponent />
+
+            </div>
+
+            <Route exact className="active" path="/" component={ Header } />
+            <Route path="/about" component={ About } />
             <TopbarComponent />
-        </div>
+        </Router>
+
+
     );
   }
 }
+
+function Header() {
+    return (
+        <HeaderComponent />
+    );
+}
+
+
+function About() {
+    return (
+        <AboutComponent />
+    );
+}
+
 
 export default App;
