73d10276ca9199d351430089eba2893944da46b1
Luis Aponte <luemapont@gmail.com>
2019-06-25 08:33:38 -0400

    Import preloader and trasition, and apply it to the components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,13 @@ import React, { Component } from "react";
 import {Helmet} from "react-helmet";
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
+//Import animations
+
+import { MDBAnimation } from "mdbreact";
+
+//Import preloaders
+import { Circle } from 'react-preloaders';
+
 //Pages Components
 import AboutComponent from './components/pagescomponents/AboutComponent';
 
@@ -11,10 +18,7 @@ import NavbarComponent from './components/NavbarComponent';
 import TopbarComponent from './components/TopbarComponent';
 import HeaderComponent from './components/HeaderComponent';
 
-var title = 'The Realstate Project | ';
-var subtitle = 'Buy your dreams!';
-//var site_url = window.location.href;
-var page_title = title + subtitle;
+var title = 'The Realstate Project';
 
 class App extends Component {
   render() {
@@ -24,9 +28,9 @@ class App extends Component {
             <div>
                 <Helmet>
                     <meta charSet="utf-8" />
-                    <title>{ page_title }</title>
+                    <title>Home - {title}</title>
                     <meta name="description" content="Realstate project made with React Js and Love." />
-                    <meta name="keywords" cpntent="realstate" />
+                    <meta name="keywords" content="realstate" />
                 </Helmet>
                 <NavbarComponent />
 
@@ -34,8 +38,11 @@ class App extends Component {
 
             <Route exact className="active" path="/" component={ Header } />
             <Route path="/about" component={ About } />
+            <Route path="/contact" component={ Contact } />
             <TopbarComponent />
+            <Circle color={'#ff7043'} time={800} />
         </Router>
+
 
 
     );
@@ -44,14 +51,34 @@ class App extends Component {
 
 function Header() {
     return (
-        <HeaderComponent />
+        <MDBAnimation type="fadeIn" duration="1s">
+            <HeaderComponent />
+        </MDBAnimation>
     );
 }
 
 
 function About() {
     return (
-        <AboutComponent />
+        <MDBAnimation type="fadeIn" duration="1s">
+            <div>
+                <Helmet>
+                    <title>About - {title}</title>
+                </Helmet>
+                <AboutComponent />
+            </div>
+        </MDBAnimation>
+    );
+}
+
+function Contact() {
+    return(
+        <MDBAnimation type="fadeIn" duration="1s">
+        <div className="my-5">
+            <h2 className="flex-center">Contact</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium asperiores beatae cum deserunt ea eligendi et ipsum labore, laudantium libero mollitia necessitatibus nemo nostrum officiis qui quia similique soluta tempore?</p>
+        </div>
+        </MDBAnimation>
     );
 }
 
